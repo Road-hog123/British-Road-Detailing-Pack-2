Road-hog123/British-Road-Detailing-Pack-2

commit c399e185541684e28ed05d76ac2b685cdea01847

Work-around for text-overflow map-bricking bug

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_1line_ARoad_Left.sco
@@ -11,7 +11,7 @@ A-Road Flag-type Direction (1 Destination, Left)
 [texttexture_enh]
 0
 Transport Medium
-480
+637
 64
 0
 255
@@ -23,7 +23,7 @@ Transport Medium
 [texttexture_enh]
 1
 Transport Medium
-93
+637
 64
 0
 255

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_1line_ARoad_Right.sco
@@ -11,7 +11,7 @@ A-Road Flag-type Direction (1 Destination, Right)
 [texttexture_enh]
 0
 Transport Medium
-480
+637
 64
 0
 255
@@ -23,7 +23,7 @@ Transport Medium
 [texttexture_enh]
 1
 Transport Medium
-93
+637
 64
 0
 255

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_1line_Left.sco
@@ -11,7 +11,7 @@ Flag-type Direction (1 Destination, Left)
 [texttexture_enh]
 0
 Transport Heavy
-480
+637
 64
 0
 35
@@ -23,7 +23,7 @@ Transport Heavy
 [texttexture_enh]
 1
 Transport Heavy
-93
+637
 64
 0
 35

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_1line_Right.sco
@@ -11,7 +11,7 @@ Flag-type Direction (1 Destination, Right)
 [texttexture_enh]
 0
 Transport Heavy
-480
+637
 64
 0
 35
@@ -23,7 +23,7 @@ Transport Heavy
 [texttexture_enh]
 1
 Transport Heavy
-93
+637
 64
 0
 35

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_2line_ARoad_Left.sco
@@ -11,7 +11,7 @@ A-Road Flag-type Direction (2 Destinations, Left)
 [texttexture_enh]
 0
 Transport Medium
-480
+637
 64
 0
 255
@@ -23,7 +23,7 @@ Transport Medium
 [texttexture_enh]
 1
 Transport Medium
-93
+637
 64
 0
 255
@@ -35,7 +35,7 @@ Transport Medium
 [texttexture_enh]
 2
 Transport Medium
-480
+637
 64
 0
 255
@@ -47,7 +47,7 @@ Transport Medium
 [texttexture_enh]
 3
 Transport Medium
-93
+637
 64
 0
 255

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_2line_ARoad_Right.sco
@@ -11,7 +11,7 @@ A-Road Flag-type Direction (2 Destinations, Right)
 [texttexture_enh]
 0
 Transport Medium
-480
+637
 64
 0
 255
@@ -23,7 +23,7 @@ Transport Medium
 [texttexture_enh]
 1
 Transport Medium
-93
+637
 64
 0
 255
@@ -35,7 +35,7 @@ Transport Medium
 [texttexture_enh]
 2
 Transport Medium
-480
+637
 64
 0
 255
@@ -47,7 +47,7 @@ Transport Medium
 [texttexture_enh]
 3
 Transport Medium
-93
+637
 64
 0
 255

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_2line_Left.sco
@@ -11,7 +11,7 @@ Flag-type Direction (2 Destinations, Left)
 [texttexture_enh]
 0
 Transport Heavy
-480
+637
 64
 0
 35
@@ -23,7 +23,7 @@ Transport Heavy
 [texttexture_enh]
 1
 Transport Heavy
-93
+637
 64
 0
 35
@@ -35,7 +35,7 @@ Transport Heavy
 [texttexture_enh]
 2
 Transport Heavy
-480
+637
 64
 0
 35
@@ -47,7 +47,7 @@ Transport Heavy
 [texttexture_enh]
 3
 Transport Heavy
-93
+637
 64
 0
 35

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_2line_Right.sco
@@ -11,7 +11,7 @@ Flag-type Direction (2 Destinations, Right)
 [texttexture_enh]
 0
 Transport Heavy
-480
+637
 64
 0
 35
@@ -23,7 +23,7 @@ Transport Heavy
 [texttexture_enh]
 1
 Transport Heavy
-93
+637
 64
 0
 35
@@ -35,7 +35,7 @@ Transport Heavy
 [texttexture_enh]
 2
 Transport Heavy
-480
+637
 64
 0
 35
@@ -47,7 +47,7 @@ Transport Heavy
 [texttexture_enh]
 3
 Transport Heavy
-93
+637
 64
 0
 35

commit b4d19ddec07153ba5efc2d55cb56a3db01ab36fa

Fix incorrect text-texture material reference caused by c399e18

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_1line_ARoad_Left.sco
@@ -43,8 +43,8 @@ Finger1A.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger1A.tga
+1
 
 [useTextTexture]
 1
@@ -53,8 +53,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger1A.tga
+2
 
 [useTextTexture]
 0

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_1line_ARoad_Right.sco
@@ -43,8 +43,8 @@ Finger1A.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger1A.tga
+1
 
 [useTextTexture]
 1
@@ -53,8 +53,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger1A.tga
+2
 
 [useTextTexture]
 0

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_1line_Left.sco
@@ -43,8 +43,8 @@ Finger1.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger1.tga
+1
 
 [useTextTexture]
 1
@@ -53,8 +53,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger1.tga
+2
 
 [useTextTexture]
 0

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_1line_Right.sco
@@ -43,8 +43,8 @@ Finger1.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger1.tga
+1
 
 [useTextTexture]
 1
@@ -53,8 +53,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger1.tga
+2
 
 [useTextTexture]
 0

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_2line_ARoad_Left.sco
@@ -67,8 +67,8 @@ Finger2A.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger2A.tga
+1
 
 [useTextTexture]
 1
@@ -77,8 +77,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger2A.tga
+2
 
 [useTextTexture]
 0
@@ -87,18 +87,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
-
-[useTextTexture]
-2
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger2A.tga
 3
+
+[useTextTexture]
+2
+
+[matl_alpha]
+2
+
+[matl]
+Finger2A.tga
+4
 
 [useTextTexture]
 3

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_2line_ARoad_Right.sco
@@ -67,8 +67,8 @@ Finger2A.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger2A.tga
+1
 
 [useTextTexture]
 1
@@ -77,8 +77,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger2A.tga
+2
 
 [useTextTexture]
 0
@@ -87,18 +87,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
-
-[useTextTexture]
-2
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger2A.tga
 3
+
+[useTextTexture]
+2
+
+[matl_alpha]
+2
+
+[matl]
+Finger2A.tga
+4
 
 [useTextTexture]
 3

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_2line_Left.sco
@@ -67,8 +67,8 @@ Finger2.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger2.tga
+1
 
 [useTextTexture]
 1
@@ -77,8 +77,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger2.tga
+2
 
 [useTextTexture]
 0
@@ -87,18 +87,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
-
-[useTextTexture]
-2
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger2.tga
 3
+
+[useTextTexture]
+2
+
+[matl_alpha]
+2
+
+[matl]
+Finger2.tga
+4
 
 [useTextTexture]
 3

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_2line_Right.sco
@@ -67,8 +67,8 @@ Finger2.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger2.tga
+1
 
 [useTextTexture]
 1
@@ -77,8 +77,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger2.tga
+2
 
 [useTextTexture]
 0
@@ -87,18 +87,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
-
-[useTextTexture]
-2
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger2.tga
 3
+
+[useTextTexture]
+2
+
+[matl_alpha]
+2
+
+[matl]
+Finger2.tga
+4
 
 [useTextTexture]
 3

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_3line_ARoad_Left.sco
@@ -91,8 +91,8 @@ Finger3A.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger3A.tga
+1
 
 [useTextTexture]
 1
@@ -101,8 +101,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger3A.tga
+2
 
 [useTextTexture]
 0
@@ -111,8 +111,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
+Finger3A.tga
+3
 
 [useTextTexture]
 2
@@ -121,28 +121,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger3A.tga
 4
 
 [useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+Finger3A.tga
+5
+
+[useTextTexture]
 5
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-5
+Finger3A.tga
+6
 
 [useTextTexture]
 4

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_3line_ARoad_Right.sco
@@ -91,8 +91,8 @@ Finger3A.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger3A.tga
+1
 
 [useTextTexture]
 1
@@ -101,8 +101,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger3A.tga
+2
 
 [useTextTexture]
 0
@@ -111,8 +111,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
+Finger3A.tga
+3
 
 [useTextTexture]
 2
@@ -121,28 +121,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger3A.tga
 4
 
 [useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+Finger3A.tga
+5
+
+[useTextTexture]
 5
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-5
+Finger3A.tga
+6
 
 [useTextTexture]
 4

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_3line_Left.sco
@@ -91,8 +91,8 @@ Finger3.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger3.tga
+1
 
 [useTextTexture]
 1
@@ -101,8 +101,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger3.tga
+2
 
 [useTextTexture]
 0
@@ -111,8 +111,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
+Finger3.tga
+3
 
 [useTextTexture]
 2
@@ -121,28 +121,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger3.tga
 4
 
 [useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+Finger3.tga
+5
+
+[useTextTexture]
 5
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-5
+Finger3.tga
+6
 
 [useTextTexture]
 4

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_3line_Right.sco
@@ -91,8 +91,8 @@ Finger3.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger3.tga
+1
 
 [useTextTexture]
 1
@@ -101,8 +101,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger3.tga
+2
 
 [useTextTexture]
 0
@@ -111,8 +111,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
+Finger3.tga
+3
 
 [useTextTexture]
 2
@@ -121,28 +121,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger3.tga
 4
 
 [useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+Finger3.tga
+5
+
+[useTextTexture]
 5
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-5
+Finger3.tga
+6
 
 [useTextTexture]
 4

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_4line_ARoad_Left.sco
@@ -115,8 +115,8 @@ Finger4A.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger4A.tga
+1
 
 [useTextTexture]
 1
@@ -125,8 +125,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger4A.tga
+2
 
 [useTextTexture]
 0
@@ -135,8 +135,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
+Finger4A.tga
+3
 
 [useTextTexture]
 2
@@ -145,18 +145,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger4A.tga
 4
+
+[useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+Finger4A.tga
+5
 
 [useTextTexture]
 5
@@ -165,28 +165,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-5
-
-[useTextTexture]
-4
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger4A.tga
 6
 
 [useTextTexture]
+4
+
+[matl_alpha]
+2
+
+[matl]
+Finger4A.tga
+7
+
+[useTextTexture]
 7
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-7
+Finger4A.tga
+8
 
 [useTextTexture]
 6

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_4line_ARoad_Right.sco
@@ -115,8 +115,8 @@ Finger4A.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger4A.tga
+1
 
 [useTextTexture]
 1
@@ -125,8 +125,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger4A.tga
+2
 
 [useTextTexture]
 0
@@ -135,8 +135,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
+Finger4A.tga
+3
 
 [useTextTexture]
 2
@@ -145,18 +145,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger4A.tga
 4
+
+[useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+Finger4A.tga
+5
 
 [useTextTexture]
 5
@@ -165,28 +165,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-5
-
-[useTextTexture]
-4
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger4A.tga
 6
 
 [useTextTexture]
+4
+
+[matl_alpha]
+2
+
+[matl]
+Finger4A.tga
+7
+
+[useTextTexture]
 7
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-7
+Finger4A.tga
+8
 
 [useTextTexture]
 6

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_4line_Left.sco
@@ -115,8 +115,8 @@ Finger4.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger4.tga
+1
 
 [useTextTexture]
 1
@@ -125,8 +125,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger4.tga
+2
 
 [useTextTexture]
 0
@@ -135,8 +135,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
+Finger4.tga
+3
 
 [useTextTexture]
 2
@@ -145,18 +145,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger4.tga
 4
+
+[useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+Finger4.tga
+5
 
 [useTextTexture]
 5
@@ -165,28 +165,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-5
-
-[useTextTexture]
-4
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger4.tga
 6
 
 [useTextTexture]
+4
+
+[matl_alpha]
+2
+
+[matl]
+Finger4.tga
+7
+
+[useTextTexture]
 7
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-7
+Finger4.tga
+8
 
 [useTextTexture]
 6

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Finger_4line_Right.sco
@@ -115,8 +115,8 @@ Finger4.tga
 1
 
 [matl]
-TextField.bmp
-0
+Finger4.tga
+1
 
 [useTextTexture]
 1
@@ -125,8 +125,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-1
+Finger4.tga
+2
 
 [useTextTexture]
 0
@@ -135,8 +135,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-2
+Finger4.tga
+3
 
 [useTextTexture]
 2
@@ -145,18 +145,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger4.tga
 4
+
+[useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+Finger4.tga
+5
 
 [useTextTexture]
 5
@@ -165,28 +165,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-5
-
-[useTextTexture]
-4
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+Finger4.tga
 6
 
 [useTextTexture]
+4
+
+[matl_alpha]
+2
+
+[matl]
+Finger4.tga
+7
+
+[useTextTexture]
 7
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-7
+Finger4.tga
+8
 
 [useTextTexture]
 6

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Route_Confirm_1line.sco
@@ -53,28 +53,28 @@ RouteConfirm1.tga
 1
 
 [matl]
-TextField.bmp
-0
-
-[useTextTexture]
-0
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm1.tga
 1
 
 [useTextTexture]
+0
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm1.tga
+2
+
+[useTextTexture]
 2
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-2
+RouteConfirm1.tga
+3
 
 [useTextTexture]
 1

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Route_Confirm_1line_ARoad.sco
@@ -53,28 +53,28 @@ RouteConfirm1A.tga
 1
 
 [matl]
-TextField.bmp
-0
-
-[useTextTexture]
-0
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm1A.tga
 1
 
 [useTextTexture]
+0
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm1A.tga
+2
+
+[useTextTexture]
 2
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-2
+RouteConfirm1A.tga
+3
 
 [useTextTexture]
 1

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Route_Confirm_2line.sco
@@ -77,28 +77,28 @@ RouteConfirm2.tga
 1
 
 [matl]
-TextField.bmp
-0
-
-[useTextTexture]
-0
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm2.tga
 1
 
 [useTextTexture]
+0
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm2.tga
+2
+
+[useTextTexture]
 2
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-2
+RouteConfirm2.tga
+3
 
 [useTextTexture]
 1
@@ -107,18 +107,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm2.tga
 4
+
+[useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm2.tga
+5
 
 [useTextTexture]
 4

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Route_Confirm_2line_ARoad.sco
@@ -77,28 +77,28 @@ RouteConfirm2A.tga
 1
 
 [matl]
-TextField.bmp
-0
-
-[useTextTexture]
-0
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm2A.tga
 1
 
 [useTextTexture]
+0
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm2A.tga
+2
+
+[useTextTexture]
 2
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-2
+RouteConfirm2A.tga
+3
 
 [useTextTexture]
 1
@@ -107,18 +107,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
-
-[useTextTexture]
-3
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm2A.tga
 4
+
+[useTextTexture]
+3
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm2A.tga
+5
 
 [useTextTexture]
 4

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Route_Confirm_3line.sco
@@ -101,28 +101,28 @@ RouteConfirm3.tga
 1
 
 [matl]
-TextField.bmp
-0
-
-[useTextTexture]
-0
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm3.tga
 1
 
 [useTextTexture]
+0
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm3.tga
+2
+
+[useTextTexture]
 2
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-2
+RouteConfirm3.tga
+3
 
 [useTextTexture]
 1
@@ -131,8 +131,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
+RouteConfirm3.tga
+4
 
 [useTextTexture]
 3
@@ -141,28 +141,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-4
-
-[useTextTexture]
-4
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm3.tga
 5
 
 [useTextTexture]
+4
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm3.tga
+6
+
+[useTextTexture]
 6
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-6
+RouteConfirm3.tga
+7
 
 [useTextTexture]
 5

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Route_Confirm_3line_ARoad.sco
@@ -101,28 +101,28 @@ RouteConfirm3A.tga
 1
 
 [matl]
-TextField.bmp
-0
-
-[useTextTexture]
-0
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm3A.tga
 1
 
 [useTextTexture]
+0
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm3A.tga
+2
+
+[useTextTexture]
 2
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-2
+RouteConfirm3A.tga
+3
 
 [useTextTexture]
 1
@@ -131,8 +131,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
+RouteConfirm3A.tga
+4
 
 [useTextTexture]
 3
@@ -141,28 +141,28 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-4
-
-[useTextTexture]
-4
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm3A.tga
 5
 
 [useTextTexture]
+4
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm3A.tga
+6
+
+[useTextTexture]
 6
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-6
+RouteConfirm3A.tga
+7
 
 [useTextTexture]
 5

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Route_Confirm_4line.sco
@@ -125,28 +125,28 @@ RouteConfirm4.tga
 1
 
 [matl]
-TextField.bmp
-0
-
-[useTextTexture]
-0
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm4.tga
 1
 
 [useTextTexture]
+0
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm4.tga
+2
+
+[useTextTexture]
 2
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-2
+RouteConfirm4.tga
+3
 
 [useTextTexture]
 1
@@ -155,8 +155,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
+RouteConfirm4.tga
+4
 
 [useTextTexture]
 3
@@ -165,18 +165,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-4
-
-[useTextTexture]
-4
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm4.tga
 5
+
+[useTextTexture]
+4
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm4.tga
+6
 
 [useTextTexture]
 6
@@ -185,8 +185,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-6
+RouteConfirm4.tga
+7
 
 [useTextTexture]
 5
@@ -195,18 +195,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-7
-
-[useTextTexture]
-7
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm4.tga
 8
+
+[useTextTexture]
+7
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm4.tga
+9
 
 [useTextTexture]
 8

OMSI 2/Sceneryobjects/Road-hog123/Addon_British-Style_Road_Signs/Generic/Directional/Route_Confirm_4line_ARoad.sco
@@ -125,28 +125,28 @@ RouteConfirm4A.tga
 1
 
 [matl]
-TextField.bmp
-0
-
-[useTextTexture]
-0
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm4A.tga
 1
 
 [useTextTexture]
+0
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm4A.tga
+2
+
+[useTextTexture]
 2
 
 [matl_alpha]
 2
 
 [matl]
-TextField.bmp
-2
+RouteConfirm4A.tga
+3
 
 [useTextTexture]
 1
@@ -155,8 +155,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-3
+RouteConfirm4A.tga
+4
 
 [useTextTexture]
 3
@@ -165,18 +165,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-4
-
-[useTextTexture]
-4
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm4A.tga
 5
+
+[useTextTexture]
+4
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm4A.tga
+6
 
 [useTextTexture]
 6
@@ -185,8 +185,8 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-6
+RouteConfirm4A.tga
+7
 
 [useTextTexture]
 5
@@ -195,18 +195,18 @@ TextField.bmp
 2
 
 [matl]
-TextField.bmp
-7
-
-[useTextTexture]
-7
-
-[matl_alpha]
-2
-
-[matl]
-TextField.bmp
+RouteConfirm4A.tga
 8
+
+[useTextTexture]
+7
+
+[matl_alpha]
+2
+
+[matl]
+RouteConfirm4A.tga
+9
 
 [useTextTexture]
 8